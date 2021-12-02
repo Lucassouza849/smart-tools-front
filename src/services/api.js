9de56fe3869d3586b110c7fafeccef272f4df521
Lucas Silva <lucas.silva@valemobi.com.br>
2021-12-02 17:34:50 -0300

    ajustando baseUrl de novo

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export const api = axios.create({
-    baseURL: 'https://smarttools-1638417877322.azurewebsites.net/'
+    baseURL: 'http://localhost:8080/'
 })
 
 export default api;
